3f0f49dd072ac22ed62cd56ad66175dc45a3779d
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-10-06 14:40:34 +0530

    Minor Changes

--- src/components/Environment.js
+++ src/components/Environment.js
@@ -9,14 +9,14 @@ const Environment = () => {
   const { ref: myRef, inView: myElementIsVisible } = useInView({triggerOnce: true});
 
   return (
-    <div id="showcase" className="pt-24 text-secondary">
+    <div id="showcase" className="pt-24 pb-2 text-secondary">
       <section className="max-w-screen-xl mx-auto px-6 md:px-12 2xl:px-0">
         <article ref={myRef} className={`fade-in space-y-24 md:space-y-0 flex flex-col md:flex-row items-center ${myElementIsVisible ? "appear" : ""}`}>
           <div className="space-y-12 text-center md:text-left">
             <h2 className="text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-medium">For every 5 websites delivered, I plant a tree!</h2>
             <p className="text-3xl font-jost">Share this website if you want me to plant more.</p>
           </div>
-          <img src={envDoodle} alt="doodle" className="sm:w-4/6 mx-auto"/>
+          <img src={envDoodle} alt="doodle" className="md:w-3/6 mx-auto"/>
         </article>
       </section>
     </div>

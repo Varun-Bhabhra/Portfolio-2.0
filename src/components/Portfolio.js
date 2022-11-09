8f4b363596cfd27da5f78b35263e2026a9b76f27
Varun Bhabhra <75928040+Varun-Bhabhra@users.noreply.github.com>
2022-11-09 19:03:39 +0530

    Portfolio Section Update

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -22,25 +22,13 @@ const Portfolio = () => {
       <section id="portfolio" className="max-w-screen-xl mx-auto px-6 md:px-12 2xl:px-0 space-y-32">
         <article className="space-y-24 md:space-y-0 md:grid grid-cols-2">
           <div id="header" className="">
-            <div ref={myRef} className={`fade-in md:sticky top-36 space-y-12 bg-portfolio-bg bg-contain bg-no-repeat text-center md:text-left ${myElementIsVisible ? "appear" : ""}`}>
+            <div ref={myRef} className={`fade-in md:sticky top-36 space-y-8 bg-portfolio-bg bg-contain bg-no-repeat text-center md:text-left ${myElementIsVisible ? "appear" : ""}`}>
               <h2 className="font-bold text-4xl md:text-4xl lg:text-5xl xl:text-6xl">Portfolio</h2>
               <p className="font-jost font-regular text-base lg:text-lg sm:w-5/6 mx-auto md:mx-0">I’ve worked with some great clients, but I won’t blab about them all. Here are a few best bits.</p>
 
               <img src={ portfolioDoodle } alt="doodle" className="mx-auto md:mx-0 -scale-x-100"/>
               <div className="hidden md:block sm:w-5/6 mx-auto md:mx-0">
-                <h2 className="font-light text-lg md:text-xl lg:text-2xl xl:text-3xl">All the sites are built from scratch in the following stack:</h2>
-                <TechItem
-                  item="HTML"
-                  className="text-[#E44D26] border-[#E44D26]"
-                />
-                <TechItem
-                  item="CSS"
-                  className="text-[#2965F1] border-[#2965F1]"
-                />
-                <TechItem
-                  item="JavaScript"
-                  className="text-[#CDB91A] border-[#CDB91A]"
-                />
+                <h2 className="font-light text-lg lg:text-xl xl:text-2xl">All the sites on your right are built from scratch in the following stack:</h2>
                 <TechItem
                   item="React Js"
                   className="text-[#00D8FF] border-[#00D8FF]"
@@ -53,11 +41,23 @@ const Portfolio = () => {
                   item="Tailwind CSS"
                   className="text-[#16BDB8] border-[#16BDB8]"
                 />
+                <TechItem
+                  item="HTML"
+                  className="text-[#E44D26] border-[#E44D26]"
+                />
+                <TechItem
+                  item="CSS"
+                  className="text-[#2965F1] border-[#2965F1]"
+                />
+                <TechItem
+                  item="JavaScript"
+                  className="text-[#CDB91A] border-[#CDB91A]"
+                />
               </div>
             </div>
           </div>
 
-          <div className="space-y-12 lg:px-4">
+          <div className="space-y-20 lg:px-4">
             <PortfolioCard
               head="My Disability Partner"
               para="MDP's role is to implement the National Disability Insurance Scheme (NDIS), which will support a better life for thousands of Australians with a significant and permanent disability and their families and carers."
@@ -66,44 +66,33 @@ const Portfolio = () => {
             />
             <PortfolioCard
               head="The Shades of Trades"
-              para="A trading course website which simplifies trading with intrinsic concepts of price action techniques and procedures."
+              para="Get a head start in your trading career and learn a skill for a
+              lifetime with The Shades of Trades.A trading course website which simplifies trading with intrinsic concepts of price action techniques and procedures."
               href="http://theshadesoftrades.co.in"
               mockup={tsot}
             />
             <PortfolioCard
               head="Zai Makeover Studio"
-              para="A portfolio and services website of a makeupartist from Chennai."
+              para="Get a classy look with Aishwarya from Zai, a portfolio/service website of an amazingly talented makeup artist."
               href="http://zaimakeoverstudio.com"
               mockup={zai}
             />
             <PortfolioCard
               head="Pyalee"
-              para="Landing page for a Tea Bag and Tea Cup company."
+              para="India's one of the best Tea Bag & Tea Cup company."
               href="http://pyalee.co.in"
               mockup={pyalee}
             />
             <PortfolioCard
-              head="Varun Bhabhra Portfolio"
-              para="My portfolio having the required information about me and the services I provide."
+              head="My Portfolio"
+              para="A freelance developer portfolio website for Mr. Varun Bhabhra providing services like website development, UI design, content writing and more."
               href="http://varunbhabhra.com"
               mockup={my}
             />
           </div>
 
           <div className="md:hidden sm:w-5/6 mx-auto md:mx-0 text-center">
-            <h2 className="font-light text-lg md:text-xl lg:text-2xl xl:text-3xl">All the sites are built from scratch in the following stack:</h2>
-            <TechItem
-              item="HTML"
-              className="text-[#E44D26] border-[#E44D26]"
-            />
-            <TechItem
-              item="CSS"
-              className="text-[#2965F1] border-[#2965F1]"
-            />
-            <TechItem
-              item="JavaScript"
-              className="text-[#CDB91A] border-[#CDB91A]"
-            />
+            <h2 className="font-light text-lg">All the above sites are built from scratch in the following stack:</h2>
             <TechItem
               item="React Js"
               className="text-[#00D8FF] border-[#00D8FF]"
@@ -115,6 +104,18 @@ const Portfolio = () => {
             <TechItem
               item="Tailwind CSS"
               className="text-[#16BDB8] border-[#16BDB8]"
+            />
+            <TechItem
+              item="HTML"
+              className="text-[#E44D26] border-[#E44D26]"
+            />
+            <TechItem
+              item="CSS"
+              className="text-[#2965F1] border-[#2965F1]"
+            />
+            <TechItem
+              item="JavaScript"
+              className="text-[#CDB91A] border-[#CDB91A]"
             />
           </div>
         </article>
